06001103e5f9a0fcb2fcd0e5eb4e045f1121a145
Animesh120503 <pradhananimesh2003@gmail.com>
2023-09-17 04:33:16 +0530

    Changed navigation menu

--- my-react/src/components/Header.js
+++ my-react/src/components/Header.js
@@ -1,54 +1,72 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.gif";
 import { HiOutlineUserCircle } from "react-icons/hi";
-import { BsCartFill } from "react-icons/bs"
+import { BsCartFill } from "react-icons/bs";
 
 const Header = () => {
-    const [showMenu, setShowMenu] = useState(false);
-    const handleShowMenu =()=>{
-        setShowMenu(preve=> !preve)
-    }
-    return (
-        <header className='fixed shadow-md w-full h-15 px-2 md: px-4 z-50 bg-white'>
-            <div className='flex items-center h-full justify-between'>
-                <Link to={""}>
-                    <div className='h-16'>
-                        <img src={logo} className="h-full" />
-                    </div>
-                </Link>
+  const [showMenu, setShowMenu] = useState(false);
+  const handleShowMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
 
-                <div className='flex items-center gap-5 md:gap-8'>
-                    <nav className='flex gap-5 md:gap-8 text-base md:text-md'>
-                        <Link to={""}>Home</Link>
-                        <Link to={"classroom"}>Classroom</Link>
-                        <Link to={"purchase"}>Purchase</Link>
-                        <Link to={"about"}>About</Link>
-                        <Link to={"contact"}>Contact</Link>
-                    </nav>
-                    <div className='text-2xl text-black-600 relative'>
-                        <BsCartFill />
-                        <div className='absolute -top-2 -right-2 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-xs text-center'>0</div>
-                    </div>
-                    <div className=' text-black-600' onClick={handleShowMenu}>
-                        <div className='text-3xl cursor-pointer' >
-                            <HiOutlineUserCircle />
-                        </div>
-                        {
-                            showMenu && (<div className='absolute right-2 bg-white py-2 px-2 shadow drop-shadow-md flex flex-col'>
-                                <Link to={"loginS"} className='whitespace-nowrap cursor-pointer'>Student</Link>
-                                <Link to={"loginT"} className='whitespace-nowrap cursor-pointer'>Teacher</Link>
-                            </div>)
-                        }
+  return (
+    <header className="fixed shadow-md w-full h-20 px-2 md:px-4 z-50 bg-white">
+      <div className="flex items-center h-full justify-between">
+        <Link to={""}>
+          <div className="h-16">
+            <img src={logo} alt="Logo" className="h-full" />
+          </div>
+        </Link>
 
-                    </div>
-                </div>
+        <div className="flex items-center gap-5 md:gap-8">
+          <nav className="flex gap-5 md:gap-8 text-base md:text-md">
+            <Link to={""} className="nav-link">
+              Home
+            </Link>
+            <Link to={"classroom"} className="nav-link">
+              Classroom
+            </Link>
+            <Link to={"purchase"} className="nav-link">
+              Purchase
+            </Link>
+            <Link to={"about"} className="nav-link">
+              About
+            </Link>
+            <Link to={"contact"} className="nav-link">
+              Contact
+            </Link>
+          </nav>
+          <div className="text-2xl text-black-600 relative">
+            <BsCartFill />
+            <div className="absolute -top-2 -right-2 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-xs text-center">
+              0
             </div>
+          </div>
+          <div className="text-black-600 relative">
+            <div
+              className={`text-3xl cursor-pointer ${showMenu ? "active" : ""}`}
+              onClick={handleShowMenu}
+            >
+              <HiOutlineUserCircle />
+            </div>
+            <div
+              className={`absolute right-2 bg-white py-2 px-2 shadow drop-shadow-md flex flex-col ${
+                showMenu ? "show" : ""
+              }`}
+            >
+              <Link to={"loginS"} className="whitespace-nowrap cursor-pointer">
+                Student
+              </Link>
+              <Link to={"loginT"} className="whitespace-nowrap cursor-pointer">
+                Teacher
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-
-
-        </header>
-    )
-}
-
-export default Header
+export default Header;
--- my-react/src/page/Home.js
+++ my-react/src/page/Home.js
@@ -1,48 +1,46 @@
-import React from 'react';
+import React from "react";
 import bkg from "../assets/images.jpg";
-import './home.css';
-import book from '../assets/book.svg';
-import book2 from '../assets/book2.svg';
-import professor from '../assets/professor.svg';
-import diploma from '../assets/diploma.svg';
+import "./home.css";
+import book from "../assets/book.svg";
+import book2 from "../assets/book2.svg";
+import professor from "../assets/professor.svg";
+import diploma from "../assets/diploma.svg";
 
 const Home = () => {
   return (
     <>
-    {/* Background image */}
-      <div className='bkgimg'>
-        <img src={bkg}/>
-        <div className='text'>
-        Get selected in every exam
-        </div>
-        <div className='btn'>
-          <button >Contact us</button>
+      {/* Background image */}
+      <div className="bkgimg">
+        <img src={bkg} />
+        <div className="text">Get selected in every exam</div>
+        <div className="btn">
+          <button>Contact us</button>
         </div>
 
-      {/* card number 1 */}
-        <div className='card1' id='c1'>
-          <img src={book} alt='book' className='card-img' />          
-          <div className='card-txt'>
-          <p>lorem ipsum lorem lorem </p>
-          <p>lorem ipsum lorem lorem </p>
-          <p>lorem ipsum lorem lorem </p>
+        {/* card number 1 */}
+        <div className="card1" id="c1">
+          <img src={book} alt="book" className="card-img" />
+          <div className="card-txt">
+            <p>lorem ipsum lorem lorem </p>
+            <p>lorem ipsum lorem lorem </p>
+            <p>lorem ipsum lorem lorem </p>
           </div>
         </div>
 
         {/* card number 2 */}
-        <div className='card2' id='c2'>
-          <img src={book2} alt=''/>
-          <div className='card-txt'>
-              <p>lorem ipsum lorem lorem </p>
-              <p>lorem ipsum lorem lorem </p>
-              <p>lorem ipsum lorem lorem </p>
+        <div className="card2" id="c2">
+          <img src={book2} alt="" />
+          <div className="card-txt">
+            <p>lorem ipsum lorem lorem </p>
+            <p>lorem ipsum lorem lorem </p>
+            <p>lorem ipsum lorem lorem </p>
           </div>
         </div>
 
         {/* card number 3 */}
-        <div className='card1' id='c3'>
-          <img src={diploma} alt=''/>
-          <div className='card-txt'>
+        <div className="card1" id="c3">
+          <img src={diploma} alt="" />
+          <div className="card-txt">
             <p>lorem ipsum lorem lorem </p>
             <p>lorem ipsum lorem lorem </p>
             <p>lorem ipsum lorem lorem </p>
@@ -50,19 +48,17 @@ const Home = () => {
         </div>
 
         {/* card number 4 */}
-        <div className='card2' id='c4'>
-          <img src={professor} alt=''/>
-          <div className='card-txt'>
-          <p>lorem ipsum lorem lorem </p>
-          <p>lorem ipsum lorem lorem </p>
-          <p>lorem ipsum lorem lorem </p>
+        <div className="card2" id="c4">
+          <img src={professor} alt="" />
+          <div className="card-txt">
+            <p>lorem ipsum lorem lorem </p>
+            <p>lorem ipsum lorem lorem </p>
+            <p>lorem ipsum lorem lorem </p>
           </div>
         </div>
       </div>
-
     </>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
